9a9f8fd5a63b5ae1a2f522d36b9e18920dcd0f12
hyeonjin <jhjin0630@gmail.com>
2023-07-11 17:47:51 +0900

    feat: Header 1차 부분 완성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Header from "./Header";
+
 function App() {
-  return <div>솔로프로젝트 시작</div>;
+  return (
+    <div>
+      <Header />
+      <div>솔로프로젝트 시작</div>
+    </div>
+  );
 }
 
 export default App;

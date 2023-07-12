44f37f8ab4b396079e1edc3978633551823ebfc2
hyeonjin <jhjin0630@gmail.com>
2023-07-12 17:54:25 +0900

    fix: Header[#2]

---
+++ src/components/Header.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./Header.css";
+import { Link } from "react-router-dom";
+import { AiOutlineGift, AiOutlineStar } from "react-icons/ai";
+
+function Header() {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownHandler = () => {
+    setDropdownOpen(!isDropdownOpen);
+  };
+
+  return (
+    <div className="header__container">
+      <Link to="/">
+        <div className="logo_wrapper">
+          <img className="logo" src="./logo.png"></img>
+          <span className="title">COZ Shopping</span>
+        </div>
+      </Link>
+      <img
+        className={`dropdown ${isDropdownOpen ? "open" : ""}`}
+        onClick={dropdownHandler}
+        src="./dropdown.svg"
+      ></img>
+      {isDropdownOpen && ( //드롭다운 수정 필요
+        <div className="open">
+          <ul>
+            <li className="triangle">
+              <img src="./Polygon.svg"></img>
+            </li>
+            <div className="text__container">
+              <div className="text__box">
+                <li>정현진님, 안녕하세요!</li>
+              </div>
+              <div className="text__box">
+                <Link to="/products/list">
+                  <li>
+                    <AiOutlineGift className="icon" />
+                    상품리스트 페이지
+                  </li>
+                </Link>
+              </div>
+              <div className="text__box">
+                <Link to="/bookmark">
+                  <li>
+                    <AiOutlineStar className="icon" />
+                    북마크 페이지
+                  </li>
+                </Link>
+              </div>
+            </div>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Header;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,16 +1,9 @@
 import React from "react";
-import Header from "../Header";
-import Footer from "../Footer";
-import "./Main.css";
 
 function Main() {
   return (
-    <div className="container">
-      <Header />
-      <div className="main-content">
-        <h1>메인 페이지</h1>
-      </div>
-      <Footer />
+    <div>
+      <h1>메인 페이지</h1>
     </div>
   );
 }

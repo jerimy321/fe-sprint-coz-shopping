602d185597de7bfb99e14ea4ca2bfd866d30decc
hyeonjin <jhjin0630@gmail.com>
2023-07-12 17:10:43 +0900

    fix: Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import Header from "./Header";
+import Header from "./components/Header";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import ProductList from "./pages/ProductList";
 import BookMark from "./pages/BookMark";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <Router>
+      <div className="container">
+        <Header />
+        <div className="content"></div>
+        <Footer />
+      </div>
+
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/products/list" element={<ProductList />} />

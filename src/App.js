21473729698ba23b02b031705b294ebd42786028
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2023-07-13 15:43:01 +0900

    충돌 해결?5

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import Header from "./components/Header";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./pages/Main";
+import ProductList from "./pages/ProductList";
+import BookMark from "./pages/BookMark";
+import Footer from "./components/Footer";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/products/list" element={<ProductList />} />
+            <Route path="/bookmark" element={<BookMark />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/Main.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Product from "../components/Product";
+import { useState, useEffect } from "react";
+//props 내려주기
+function Main() {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    fetch("http://cozshopping.codestates-seb.link/api/v1/products?count=4")
+      .then((res) => res.json())
+      .then((data) => {
+        setProductList(data);
+        console.log(data);
+      });
+  }, []);
+  //data.type => Exhibition, Category, Brand, Product
+  //Product => 상품 이름(title[2]), 00%(discountPercentage[6]), 상품 가격(price[5])
+  //Brand => 브랜드이름(brand_name[4]), 관심고객수(follower[9]), 브랜드 이미지(brand_image_url[8])
+  //Category => 카테고리이름(title)
+  //Exhibition => 기획전 이름(title), 기획전 설명(sub_title[3])
+  const shoppingList = () => {
+    return productList.map((item) => (
+      <div key={item.id} className="list">
+        <img src={item.image_url} alt="product" />
+        <h3>{item.title}</h3>
+      </div>
+    ));
+  };
+  return (
+    <div>
+      <h2>상품 리스트</h2>
+      <div>{shoppingList()}</div>
+      <h2>북마크 리스트</h2>
+    </div>
+  );
+}
+
+export default Main;

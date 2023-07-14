40114cfd664b5a796d366b8b16a2ff3da6685892
jerimy321 <123806723+jerimy321@users.noreply.github.com>
2023-07-14 16:50:34 +0900

    Feat: Filter 기능 완료 [#6]

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -6,53 +6,46 @@ const Filter = ({ filteredType, onTypeClick, showFilter }) => {
   const handleTypeClick = (type) => {
     onTypeClick(type);
   };
-  console.log(showFilter);
-  if (showFilter === false) {
+  const filterItems = [
+    { type: null, label: "전체", image: "/images/filter_Total.jpg" },
+    { type: "Product", label: "상품", image: "/images/filter_Product.jpg" },
+    {
+      type: "Category",
+      label: "카테고리",
+      image: "/images/filter_Category.jpg",
+    },
+    {
+      type: "Exhibition",
+      label: "기획전",
+      image: "/images/filter_Exhibition.jpg",
+    },
+    { type: "Brand", label: "브랜드", image: "/images/filter_Brand.jpg" },
+  ];
+
+  if (!showFilter) {
     return null;
   }
 
   return (
     <div className="filter__container">
-      <div
-        className={`filter__item ${filteredType === null ? "active" : ""}`}
-        onClick={() => handleTypeClick(null)}
-      >
-        <img src="/images/filter_total.jpg"></img>
-        <span>전체</span>
-      </div>
-      <div
-        className={`filter__item ${filteredType === "Product" ? "active" : ""}`}
-        onClick={() => handleTypeClick("Product")}
-      >
-        <img src="/images/filter_product.jpg"></img>
-        <span>상품</span>
-      </div>
-      <div
-        className={`filter__item ${
-          filteredType === "Category" ? "active" : ""
-        }`}
-        onClick={() => handleTypeClick("Category")}
-      >
-        <img src="/images/filter_category.jpg"></img>
-        <span>카테고리</span>
-      </div>
-      <div
-        className={`filter__item ${
-          filteredType === "Exhibition" ? "active" : ""
-        }`}
-        onClick={() => handleTypeClick("Exhibition")}
-      >
-        <img src="/images/filter_exhibition.jpg"></img>
-        <span>기획전</span>
-      </div>
-
-      <div
-        className={`filter__item ${filteredType === "Brand" ? "active" : ""}`}
-        onClick={() => handleTypeClick("Brand")}
-      >
-        <img src="/images/filter_brand.jpg"></img>
-        <span>브랜드</span>
-      </div>
+      {filterItems.map((item) => {
+        return (
+          <div
+            key={item.type}
+            className="filter__item"
+            onClick={() => handleTypeClick(item.type)}
+          >
+            <img className="filter__img" src={item.image} alt={item.label} />
+            <span
+              className={`filter__label${
+                filteredType === item.type ? " active" : ""
+              }`}
+            >
+              {item.label}
+            </span>
+          </div>
+        );
+      })}
     </div>
   );
 };
